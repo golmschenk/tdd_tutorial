e4774afc3df03faa0ccb399469768f08027c4eb1
Greg Olmschenk <golmschenk@gmail.com>
2015-09-11 16:58:17 -0400

    Create URL for adding items to an existing list

--- superlists/settings.py
+++ superlists/settings.py
@@ -101,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom
+
+TEST_RUNNER = 'rainbowtests.test.runner.RainbowDiscoverRunner'
