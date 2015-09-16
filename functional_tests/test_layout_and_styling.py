61eb2a4cbaa3c74c9c08f15a48bd6e0ad481a940
Greg Olmschenk <golmschenk@gmail.com>
2015-09-16 13:04:31 -0400

    Split functional tests into individual files

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,27 @@
+from .base import FunctionalTest
+
+
+class LayoutAndStylingTest(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        # Kara goes to the homepage.
+        self.browser.get(self.server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices a nicely centered box.
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees that the box is centered on that page as well.
+        input_box.send_keys('Just checking\n')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=5
+        )
+
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,35 +1,9 @@
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import sys
 
-class NewVisitorTest(StaticLiveServerTestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Kara heard about a new top-notch online to-do app. She goes to the website to check it out.
@@ -98,26 +72,4 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('Buy a telescope', page_text)
 
         # For the moment, both Kara and Sophia's skeptism about the site is quelled, and both go back to sleep.
-
-    def test_layout_and_styling(self):
-        # Kara goes to the homepage.
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She notices a nicely centered box.
-        input_box = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            input_box.location['x'] + input_box.size['width'] / 2,
-            512,
-            delta=5
-        )
-
-        # She starts a new list and sees that the box is centered on that page as well.
-        input_box.send_keys('Just checking\n')
-        input_box = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            input_box.location['x'] + input_box.size['width'] / 2,
-            512,
-            delta=5
-        )
 
