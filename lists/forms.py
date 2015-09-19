d19bf7be79d7c79c5be11ad58e3d6a5c024bb230
Greg Olmschenk <golmschenk@gmail.com>
2015-09-19 18:24:48 -0400

    Add non-duplicate validation form for an existing list

--- lists/forms.py
+++ lists/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
-
+DUPLICATE_ITEM_ERROR = "You already have this item in this list"
 
 class ItemForm(forms.models.ModelForm):
 
@@ -23,4 +24,18 @@ class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
+
+
+class ExistingListItemForm(ItemForm):
+
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as error:
+            error.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(error)
 
